5a9adcc9ee0fc33e95834a6c0c53416cdd544063
EliasE83 <mayorelias83@gmail.com>
2023-08-05 18:32:38 -0600

    Modulo de Alumnos, Operador e Index hechos
    Se culminarion los modulos Operador y Index, Alumnos 90%

--- app.py
+++ app.py
@@ -22,14 +22,21 @@ app.config['SQL_SERVER_URI']='Driver={SQL Server};Server=ELIAS;Database=Proyecto
 
 #Declaracion de la ruta http://localhost:5000
 @app.route('/')
-def index():
+def login():
     return render_template('login.html')
 
 #Ruta http://localhost:5000/guardar tipo POST para insert
 
 @app.route('/index')
 def inicio():
-    return render_template('index.html')
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute('select id_registroautobus, Rutas.numero, RegistroAutobuses.id_autobus, Cuatrimestres.periodo from RegistroAutobuses inner join Rutas on Rutas.id_ruta = RegistroAutobuses.id_ruta inner join Cuatrimestres on Cuatrimestres.id_cuatrimestre = RegistroAutobuses.id_cuatrimestre where RegistroAutobuses.Estatus = 1')
+    consulta = cursor.fetchall()
+
+    cursor.execute('select RegistroAutobuses.id_registroautobus ,Operadores.numeroempleado, RegistroAutobuses.id_autobus from RegistroOperadores inner join Operadores on Operadores.id_operador = RegistroOperadores.id_operador inner join RegistroAutobuses on RegistroAutobuses.id_registroautobus = RegistroOperadores.id_registroautobus where RegistroOperadores.Estatus = 1')
+    consulta2 = cursor.fetchall()
+    return render_template('index.html', lsConsulta = consulta, lsConsulta2 = consulta2)
 
 #Rutas
 
@@ -156,6 +163,159 @@ def editarbusBD(id):
         con.close()
     return redirect(url_for('consultabus'))
 
+@app.route('/asignarbus')
+def asignarbus():
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute('select id_autobus from Autobuses')
+    consultas = cursor.fetchall()
+
+    cursor.execute('select id_ruta, numero from Rutas')
+    consultas2 = cursor.fetchall()    
+
+    cursor.execute('select id_cuatrimestre, periodo from Cuatrimestres')
+    consultas3 = cursor.fetchall()
+
+    return render_template('autobuses/asignarbus.html', lsBus = consultas, lsRutas = consultas2, lsCuatri = consultas3)
+
+@app.route('/asignarbusBD', methods=['POST'])
+def asignarbusBD():
+    if request.method == 'POST':
+        vrcuatrimestre = request.form['asigper']
+        vrruta = request.form['asigrut']
+        vridbus = request.form['asigbus']
+
+        con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+        cursor = con.cursor()
+        cursor.execute('insert into RegistroAutobuses (id_cuatrimestre, id_ruta, id_autobus) values (?,?,?)', (vrcuatrimestre,vrruta,vridbus))
+        con.commit()
+        con.close()
+
+    return render_template('ventanaemergente.html')
+
+@app.route('/desactivaruta/<id>')
+def desactivaruta(id):
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute('update RegistroAutobuses set Estatus = 0 where id_registroautobus = ?', (id))
+    con.commit()
+    con.close()
+    return redirect(url_for('inicio'))
+
+#Operadores
+
+@app.route('/registroperador')
+def registroperador():
+    return render_template('operadores/registroperador.html')
+
+@app.route('/registroperadorBD', methods=['POST'])
+def registroperadorBD():
+
+    vonombre = request.form['onombre']
+    voap = request.form['oap']
+    voam = request.form['oam']
+
+    volicencia = request.form['olicencia']
+    vonumemp = request.form['onoemp']
+    
+
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute('insert into Personas (nombre, ap, am) values (?,?,?)', (vonombre,voap,voam))
+    con.commit()
+    con.close()
+        
+    con1 = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor1 = con1.cursor()   
+    cursor1.execute('select id_persona from Personas where nombre = ? and ap = ? and am = ?', (vonombre,voap,voam))
+    idpersona = cursor1.fetchone()
+    print(idpersona[0], vonombre, voap, voam, volicencia, vonumemp)
+
+    cursor1.execute('insert into Operadores (id_persona, licencia, numeroempleado) values (?,?,?)', (idpersona[0],volicencia,vonumemp))
+    con1.commit()
+    con1.close()
+
+    return render_template('ventanaemergente.html')
+
+@app.route('/consultaroperador')
+def consultaroperador():
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute("select id_operador, (Personas.nombre+' '+Personas.ap+' '+Personas.am), licencia, numeroempleado from Operadores inner join Personas on Personas.id_persona = Operadores.id_persona")
+    consultas = cursor.fetchall()
+    return render_template('operadores/consultaroperador.html', lsOperador = consultas)
+
+@app.route('/asignaroperador')
+def asignaroperador():
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute('select id_operador, numeroempleado from Operadores')
+    consultas = cursor.fetchall()
+
+    cursor.execute('select id_registroautobus, Rutas.numero from RegistroAutobuses inner join Rutas on Rutas.id_ruta = RegistroAutobuses.id_ruta where RegistroAutobuses.Estatus = 1')
+    consultas2 = cursor.fetchall()
+
+    return render_template('operadores/asignaroperador.html', lsOperadorAS = consultas, lsRegistro = consultas2)
+
+@app.route('/asignaroperadorBD', methods=['POST'])
+def asignaroperadorBD():
+    if request.method == 'POST':
+        vroperador = request.form['asigop']
+        vrregistro = request.form['asigrutop']
+
+        con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+        cursor = con.cursor()
+        cursor.execute('insert into RegistroOperadores (id_registroautobus, id_operador) values (?,?)', (vrregistro,vroperador))
+        con.commit()
+        con.close()
+
+    return render_template('ventanaemergente.html')
+
+@app.route('/desactivarop/<id>')
+def desactivarop(id):
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute('update RegistroOperadores set Estatus = 0 where id_registroautobus = ?', (id))
+    con.commit()
+    con.close()
+    return redirect(url_for('inicio'))
+
+#Alumnos
+
+@app.route('/consultalumno')
+def consultalumno():
+    con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+    cursor = con.cursor()
+    cursor.execute("select id_alumno, (Personas.nombre +' '+ Personas.ap +' '+ Personas.am), matricula from Alumnos inner join Personas on Personas.id_persona = Alumnos.id_persona")
+    consultas = cursor.fetchall()
+
+    return render_template('alumnos/consultalumno.html', lsAlumno = consultas)
+
+@app.route('/registroalumno')
+def registroalumno():
+    return render_template('alumnos/registroalumno.html')
+
+@app.route('/registroalumnoBD', methods=['POST'])
+def registroalumnoBD():
+    if request.method == 'POST':
+        vonombre = request.form['anombre']
+        voaap = request.form['aap']
+        voaam = request.form['aam']
+        vomatricula = request.form['amatricula']
+        vacarrera = request.form['acarrera']
+
+        con = pyodbc.connect(app.config['SQL_SERVER_URI'])
+        cursor = con.cursor()
+        cursor.execute('insert into Personas (nombre, ap, am) values (?,?,?)', (vonombre,voaap,voaam))
+        con.commit()
+        cursor.execute('select id_persona from Personas where nombre = ? and ap = ? and am = ?', (vonombre,voaap,voaam))
+        idpersona = cursor.fetchone()
+
+        cursor.execute('insert into Alumnos (id_persona, matricula, id_carrera) values (?,?,?)', (idpersona[0],vomatricula,vacarrera))
+        con.commit()
+        con.close()
+
+    return render_template('ventanaemergente.html')
 
 #Ejecucion de nuestro programa
 if __name__ == '__main__':
